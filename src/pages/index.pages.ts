336ba2d830d70ff8986f2ea81fd1a9de797f1866
diegosvi <dego1d@hotmail.com>
2017-07-27 22:36:11 -0500

    prueba

--- src/pages/index.pages.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Diego Villavicencio on 12/7/2017.
- */
-export {PrincipalPage} from './principal/principal';
-export {ProductosPage} from './productos/productos';
-export {PromocionesPage} from './promociones/promociones';
